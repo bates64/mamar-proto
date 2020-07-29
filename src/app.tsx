ae6e75143605d3242f8d0c6d9ea8a48b3cd6653d
Alex Bates <hi@imalex.xyz>
2020-07-29 11:49:31 +0100

    initial commit

---
+++ src/app.tsx
@@ -0,0 +1,5 @@
+export function App(props) {
+	return (
+		<p>Hello, world!</p>
+	)
+}
---
+++ vite.config.js
@@ -0,0 +1,10 @@
+// @ts-check
+const preactRefresh = require('@prefresh/vite')
+
+/** @type { import('vite').UserConfig } */
+const config = {
+	jsx: 'preact',
+	plugins: [preactRefresh()]
+}
+
+module.exports = config
